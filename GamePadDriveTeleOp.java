abdcb18cc4ea65bb614764ea1ad6532f3ccd877e
Shashin Gupta <41521876+Shashin-Gupta@users.noreply.github.com>
2019-12-30 20:49:22 -0800

    TeleOp
    Version 3.5
    
    Updated off of previous errors that were seen on the on-bot system console.
    Move back to the stick because the trigger was not working.
    
    Edited by Shashin Gupta

--- GamePadDriveTeleOp.java
+++ GamePadDriveTeleOp.java
@@ -1,6 +1,5 @@
 package com.qualcomm.robotcontroller.opmodes;
 
-import com.qualcomm.robotcore.eventloop.opmode.opMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
@@ -8,11 +7,11 @@ import com.qualcomm.robotcore.hardware.DcMotor;
  * @author Shashin Gupta
  * @author Kushagr Khanna
  * @since 9/27/2019
- * @version 3.0
+ * @version 3.5
  *
  **/
 
-public class GamePadDriveTeleOp extends OpMode {
+public class GamePadDriveTeleOp extends BasicOpMode_Linear {
 
 	DcMotor leftWheel, rightWheel, leftWheel2, rightWheel2;
 	double jointWheelPower;
@@ -22,8 +21,8 @@ public class GamePadDriveTeleOp extends OpMode {
 		leftWheel = hardwareMap.dcMotor.get("left_wheel");
 		rightWheel = hardwareMap.dcMotor.get("right_wheel");
 		
-		leftWheel = hardwareMap.dcMotor.get("left_wheel");
-		rightWheel = hardwareMap.dcMotor.get("right_wheel");
+		leftWheel2 = hardwareMap.dcMotor.get("left_wheel");
+		rightWheel2 = hardwareMap.dcMotor.get("right_wheel");
 
 		leftWheel.setDirection(DcMotor.Direction.REVERSE);
 		rightWheel2.setDirection(DcMotor.Direction.REVERSE);
@@ -32,11 +31,12 @@ public class GamePadDriveTeleOp extends OpMode {
 
 	@Override
 	public void loop() {
-		// change left stick and right stick right trigger 
-		if (gamepad1.right_trigger.isPressed()) jointWheelPower = gamepad1.right_trigger_y;
-		else if (gamepad1.left_trigger.isPressed()) jointWheelPower = -gamepad1.left_trigger_y;
+		if (gamepad1.right_stick.isPressed()) jointWheelPower = gamepad1.right_stick_y;
+		else if (gamepad1.left_stick.isPressed()) jointWheelPower = -gamepad1.left_stick_y;
 		
 		leftWheel.setPower(jointWheelPower);
 		rightWheel.setPower(jointWheelPower);
+		leftWheel2.setPower(jointWheelPower);
+		rightWheel2.setPower(jointWheelPower);
 	}
 }
